7d24aa78d17708ddcf8db162512f86468d226490
Martí Cervià Caballé <marti.cervia@gmail.com>
2019-05-03 10:30:41 +0200

    Refactor the main source file

--- WAKE_UP_DETECTOR/src/main.c
+++ WAKE_UP_DETECTOR/src/main.c
@@ -1,40 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    PWR/PWR_STOP/Src/main.c
-  * @author  MCD Application Team
-  * @brief   This sample code shows how to use STM32L0xx PWR HAL API to enter
-  * and exit the stop mode without RTC.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include <periph_config.h>
 #include "main.h"
 #include <string.h>
@@ -46,15 +9,6 @@
 COMP_HandleTypeDef hcomp1;
 TIM_HandleTypeDef  timeout_timer;
 
-/** @addtogroup STM32L0xx_HAL_Examples
-  * @{
-  */
-
-/** @addtogroup PWR_STOP
-  * @{
-  */
-
-/* Private typedef -----------------------------------------------------------*/
 typedef enum wurx_states{
 	WURX_SLEEP = 0,
 	WURX_WAITING_PREAMBLE = 1,
@@ -70,13 +24,7 @@ typedef struct wurx_context{
 	uint8_t frame_buffer[MAX_FRAME_LEN];
 }wurx_context_t;
 
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
 static volatile uint32_t timer_timeout = 0;
-/* Private function prototypes -----------------------------------------------*/
-
-/* Private functions ---------------------------------------------------------*/
 static uint16_t expected_addr[20] = {0, 0, INPUT_FAST, 0, 0,INPUT_FAST,0,INPUT_FAST,0,0,INPUT_FAST,INPUT_FAST,0,0,0,0,INPUT_FAST,INPUT_FAST,INPUT_FAST,INPUT_FAST};
 
 static void sleepMCU(wurx_context_t* wur_context){
@@ -159,14 +107,14 @@ static void loopWuR(wurx_context_t* context){
 
 		TIMER_DISABLE(TIM2);
 
-		//12 instructions loop
+		//64 instructions loop at 16MHz
 		for(uint8_t loop = 0; loop < MAX_LOOPS; loop++){
 			result = READ_PIN(GPIOA, INPUT_FAST);
 			if(result != expected_addr[loop]){
 				goToSleep(context);
 				break;
 			}
-			//52 cycles left still to do crazy shit
+			// 12 instructions used, 52 cycles left still to do crazy shit
 			ADJUST_WITH_NOPS;
 		}
 
@@ -196,6 +144,3 @@ int main(void)
 	loopWuR(&context);
 
 }
-
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
